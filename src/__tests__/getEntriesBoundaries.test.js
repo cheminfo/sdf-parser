a9506664b3feee7f2dc465e77cb86af285f341fb
Luc Patiny <luc@patiny.com>
2021-10-14 08:17:10 +0200

    test: move __tests__ folder

---
+++ src/__tests__/getEntriesBoundaries.test.js
@@ -0,0 +1,28 @@
+'use strict';
+
+let fs = require('fs');
+
+let getEntriesBoundaries = require('../src/getEntriesBoundaries');
+
+let sdf0 = fs.readFileSync(`${__dirname}/test.sdf`, 'utf-8');
+let sdf1 = fs.readFileSync(`${__dirname}/test1.sdf`, 'utf-8');
+let sdf2 = fs.readFileSync(`${__dirname}/test2.sdf`, 'utf-8');
+
+[sdf0, sdf1, sdf2].forEach((sdf) => {
+  let eol = '\n';
+  let header = sdf.substr(0, 1000);
+  if (header.indexOf('\r\n') > -1) {
+    eol = '\r\n';
+  } else if (header.indexOf('\r') > -1) {
+    eol = '\r';
+  }
+
+  test('Split should match regex behavior', function () {
+    let sdfParts = sdf.split(new RegExp(`${eol}\\$\\$\\$\\$.*${eol}`));
+    expect(sdfParts).toEqual(
+      getEntriesBoundaries(sdf, `${eol}$$$$`, eol).map((v) =>
+        sdf.substring(...v),
+      ),
+    );
+  });
+});
